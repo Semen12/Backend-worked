90f734282a826b3f1d96782fa8b7130c942b9a7e
Semen12 <kondaurov.semion@yandex.ru>
2024-04-27 23:40:32 +0300

    Создана таблица для учтных записей, а также установлены необходимые отношения с таблицами ( юзеры к учетным записям, учтные записи к конткретному пользователю)
    	modified:   app/Models/Account.php
    	modified:   app/Models/User.php
    	modified:   config/app.php
    	modified:   database/migrations/2024_04_06_151036_create_accounts_table.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'source_type',
+        'username',
+        'password',
+        'comment',
+    ];
+
+    protected function casts(): array // добавления атрибутов
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+    // Добавляем отношение к модели User
+    public function user()
+    {
+        return $this->belongsTo(User::class); // обратное отношение один к одному
+    }
+
 }
