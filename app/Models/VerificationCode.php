20d001a0b8141a2301822b5400c06e8f7e0ea562
Semen12 <kondaurov.semion@yandex.ru>
2024-05-17 16:14:10 +0300

    В контрлере MasterPassword добавли возможность установки мастер пароля, его обновление, отправки ссылки на его сброс и реалиция сброса масетр пароля. Для этого были созданы маршуты и таблица с моделью для хранения токенов. Доработана функция отправки маршутов (страя закоментина) ,в новой используется метод updateorcreate и код просто обновляются. если пользователь отправляет код ещё раз. Так же можно реализовать задачу ( и она вроде уже есть) , котрая удаляет просроченные токены, как и с кодами. Добавил проверку на владения подписью для подтверждения email и ссылкой для восстановление master-password. Создал уведомление и установил соотвествующие соотношения.

--- app/Models/VerificationCode.php
+++ app/Models/VerificationCode.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class VerificationСode extends Model
+class VerificationCode extends Model
 {
     use HasFactory;
 
